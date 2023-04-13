fbe7efd04602a53d5ed68fc8ef7cde673665955f
Kxkee <tom.cousin06@gmail.com>
2023-04-13 22:33:03 +0200

    webhook

--- components/Contact.tsx
+++ components/Contact.tsx
@@ -13,7 +13,7 @@ export default function Contact() {
         e.preventDefault();
         if(name && email && message) {
             try {
-                const response = await axios.post(`${process.env.NEXT_PUBLIC_PIPE_WEBHOOK}`, {
+                const response = await axios.post(`https://eo3uxmpgx4u9mcd.m.pipedream.net`, {
                     name,
                     email,
                     message,
